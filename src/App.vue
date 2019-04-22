c53ef346f21889e8286153356b5f45139356a725
lixingyu_price <lixingyu_price@163.com>
2019-04-22 21:15:18 +0800

    git测试

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <div>price的修改</div>
     </div>
     <router-view/>
   </div>
